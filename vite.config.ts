ad3145b5d1f99e02b531323eb875a7a9ea84fb86
Cellivar <1441553+Cellivar@users.noreply.github.com>
2024-01-05 03:05:04 -0800

    Add tests

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig({
   },
   plugins: [dts()],
   test: {
-
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   }
 });
